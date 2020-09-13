d2435158da3cbd11ed48bca92e80379194c699fd
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-09-14 01:01:06 +0530

    added ML model

---
+++ image_detect.py
@@ -0,0 +1,60 @@
+# USAGE
+# python image_detect.py --image <path>
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import cv2
+import os
+
+
+def checkmask(img_data):
+    args = {'image': img_data, 'face': 'face_detector', 'model': 'mask_detector.model', 'confidence': 0.5}
+
+
+    print(args)
+    prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+    weightsPath = os.path.sep.join([args["face"],
+                                    "res10_300x300_ssd_iter_140000.caffemodel"])
+    net = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+    model = load_model(args["model"])
+
+    image = cv2.imread(args["image"])
+    orig = image.copy()
+    (h, w) = image.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > args["confidence"]:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = image[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            (mask, withoutMask) = model.predict(face)[0]
+
+        # label = "Mask" if mask > withoutMask else "No Mask"
+        output = 1 if mask > withoutMask else 0
+
+    if output is 1:
+        return True
+    else:
+        return False
+
